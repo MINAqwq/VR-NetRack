3081d924d7c5f7936e4b5c449cdc4788e34ba744
Pixel Brush <letsplaytvirtmann@gmail.com>
2023-11-29 12:22:31 +0100

    LOTS OF FUCKSHIT OMG
    WHY THE FUCK DOES THE CABLE UNPLUGGING NOT WORK??? WHAT UNHOLY, SPITEFUL CREATOR HAS CURSED ME WITH THIS EXISTENCE AND COD??????

--- Assets/pluggable.cs
+++ Assets/pluggable.cs
@@ -1,42 +1,74 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Transformers;
 
 public class pluggable : MonoBehaviour
 {
     public GameObject self;
+    private GameObject pluggedIntoThis;
+    public TextMeshPro plugStateTextSelf;
+    public TextMeshPro plugStateTextOther;
     private Rigidbody rb;
     private XRGrabInteractable interactableGrab;
     private XRGeneralGrabTransformer interactableGrabTransformer;
+    int timeout = 0;
+    bool pluggedIntoSomething = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         interactableGrab = GetComponent<XRGrabInteractable>();
         interactableGrabTransformer = GetComponent<XRGeneralGrabTransformer>();
+        plugStateTextSelf.text = "Unplugged";
+        plugStateTextOther.text = "Unplugged";
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (pluggedIntoSomething)
+        {
+            if (timeout > 60)
+            {
+                interactableGrab.enabled = true;
+                interactableGrabTransformer.enabled = true;
+                disableAll();
+            } else { 
+                timeout++;
+            }
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Plug")
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
-            rb.isKinematic = true;
-            rb.detectCollisions = true;
-            rb.useGravity = false;
-            interactableGrab.enabled = false;
+            pluggedIntoThis = other.gameObject;
             interactableGrabTransformer.enabled = false;
-            self.transform.rotation = Quaternion.Euler(0, 0, 0);
-            self.transform.position = other.transform.position;
-            //self.transform = (collision.gameObject.transform);
-            //Debug.Log("Do something here");
+            interactableGrab.enabled = false;
+            disableAll();
         }
+    }
+
+    private void disableAll()
+    {
+        rb.detectCollisions = false;
+        rb.useGravity = false;
+
+        self.transform.rotation = Quaternion.Euler(0, 0, 0);
+        self.transform.position = pluggedIntoThis.transform.position;
+        plugStateTextSelf.text = pluggedIntoThis.name;
+        plugStateTextOther.text = pluggedIntoThis.name;
+        pluggedIntoThis.GetComponent<Collider>().enabled = false;
+
+        timeout = 0;
+        pluggedIntoSomething = true;
+        rb.isKinematic = true;
     }
 }
