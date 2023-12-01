0cff842cf929124afe36d87b80a5fc8047ecd05f
Torben.Virtmann <letsplaytvirtmann@gmail.com>
2023-12-01 11:35:42 +0100

    Actually working plugs

--- Assets/pluggable.cs
+++ Assets/pluggable.cs
@@ -1,7 +1,9 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
+using Unity.XR.CoreUtils.Bindings.Variables;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -15,59 +17,91 @@ public class pluggable : MonoBehaviour
     public TextMeshPro plugStateTextOther;
     private Rigidbody rb;
     private XRGrabInteractable interactableGrab;
-    private XRGeneralGrabTransformer interactableGrabTransformer;
-    int timeout = 0;
-    bool pluggedIntoSomething = false;
+    private bool pluggedIntoSomething = false;
+    int unplugTimeout = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         interactableGrab = GetComponent<XRGrabInteractable>();
-        interactableGrabTransformer = GetComponent<XRGeneralGrabTransformer>();
+        resetText();
+    }
+
+    private void resetText()
+    {
         plugStateTextSelf.text = "Unplugged";
         plugStateTextOther.text = "Unplugged";
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         if (pluggedIntoSomething)
         {
-            if (timeout > 60)
-            {
-                disableAll();
-            } else { 
-                timeout++;
+            // Make it grabbable again
+            interactableGrab.interactionLayers = 1;
+        } else {
+            // only run this if we've had something assigned in the past
+            if (pluggedIntoThis) {
+                // If it is no longer plugged into something, but the plug is still disabled
+                if (pluggedIntoThis.GetComponent<Collider>().enabled == false)
+                {
+                    if (unplugTimeout < 60) { 
+                        unplugTimeout++;
+                    } else {
+                        pluggedIntoThis.GetComponent<Collider>().enabled = true;
+                    }
+                }
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Plug")
+        if ((other.gameObject.tag == "Plug") && (!pluggedIntoSomething))
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
             pluggedIntoThis = other.gameObject;
-            interactableGrab.IsSelected(null);
-            interactableGrabTransformer.enabled = false;
-            interactableGrab.enabled = false;
             disableAll();
         }
     }
 
     private void disableAll()
     {
-        rb.detectCollisions = false;
-        rb.useGravity = false;
+        // Disable Grab
+        interactableGrab.interactionLayers = 0;
 
+        // Set position to plug
         self.transform.rotation = Quaternion.Euler(0, 0, 0);
         self.transform.position = pluggedIntoThis.transform.position;
+
+        // Set names
         plugStateTextSelf.text = pluggedIntoThis.name;
         plugStateTextOther.text = pluggedIntoThis.name;
+
+        // Disable plug collision
         pluggedIntoThis.GetComponent<Collider>().enabled = false;
 
-        timeout = 0;
-        pluggedIntoSomething = true;
+        // Freeze in place
         rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        pluggedIntoSomething = true;
+    }
+
+    public void enableAll()
+    {
+        if (pluggedIntoSomething) {
+            // Unfreeze etc.
+            rb.isKinematic = false;
+            rb.constraints = ~RigidbodyConstraints.FreezeAll;
+            pluggedIntoSomething = false;
+
+            // Restart unplug timer
+            unplugTimeout = 0;
+
+            // Reset Text
+            resetText();
+        }
     }
 }
